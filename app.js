63862a2ff37912d7de21a1cf545fdd9b17aaacce
Max Carlquist <tenkakle@gmail.com>
2018-02-13 07:29:28 +0100

    Implemented search functionality

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 $(function () {
 
     // Api Url for
-    var jamendoApi = 'https://api.jamendo.com/v3.0/artists/tracks/?client_id=f44f2488&format=jsonpretty';
+    var jamendoApi = 'https://api.jamendo.com/v3.0/tracks/?client_id=f44f2488&format=jsonpretty';
 
 
     /* Functions used */
 
     // display songs .music-results
-    function displaySongs (music) {
+    function displaySongs(music) {
+        console.log(music);
         for (var i = 0; i < music.length; i++) {
-            var bandName = music[i].name;
-            var tracks = music[i].tracks;
-            tracks.forEach(function (item) {
-                console.log(item);
-                var template = '<li class="songs"> <p> ' + bandName +'</p> </li>';
-                $('.songs').html(template);
-            });
+            var track = music[i].name;
+            var artist = music[i].artist_name;
+            var template = '<li class="song"><p> ' + artist + ' - <span>'+track +'</span></p><button class="listen">Listen</button></li>';
+            $('.songs').append(template);
         }
     }
 
@@ -24,7 +22,7 @@ $(function () {
         e.preventDefault();
         // create an instance of the search from input
         var search = $('.input').val();
-
+        $('.songs').html('');
         var params = {
             namesearch: search
         };
