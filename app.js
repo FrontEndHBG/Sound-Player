d4f25ef2e103c9b393ca2c9344a26939cbf696ac
Max Carlquist <tenkakle@gmail.com>
2018-02-13 08:06:55 +0100

    Implemented play function and final styles

--- app.js
+++ app.js
@@ -2,19 +2,31 @@ $(function () {
 
     // Api Url for
     var jamendoApi = 'https://api.jamendo.com/v3.0/tracks/?client_id=f44f2488&format=jsonpretty';
-
+    var audioTrack = null;
 
     /* Functions used */
-
     // display songs .music-results
     function displaySongs(music) {
-        console.log(music);
         for (var i = 0; i < music.length; i++) {
             var track = music[i].name;
             var artist = music[i].artist_name;
-            var template = '<li class="song"><p> ' + artist + ' - <span>'+track +'</span></p><button class="listen">Listen</button></li>';
+            var song = music[i].audio;
+            var template = '<li class="song"><p> ' + artist + ' - <span>'+track +'</span></p><button data-song="'+ song +'" class="listen btn">Listen</button><button class="btn pause">Pause</button></li>';
             $('.songs').append(template);
         }
+        $('.listen').click(function (e) {
+            $(this).siblings('.pause').show();
+            audioTrack = new Pizzicato.Sound(e.target.dataset.song, function () {
+                audioTrack.play();
+            });
+            $(this).hide();
+        });
+
+        $('.pause').click(function (e) {
+            audioTrack.stop();
+            $(this).hide();
+            $(this).siblings('.listen').show();
+        });
     }
 
     // Form submission
@@ -32,4 +44,5 @@ $(function () {
             displaySongs(results);
         });
     }); // end of form submission
+   
 }); // end of jQuery
